82dadcded74fede4b4896d4de83604c7d7f06d5a
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-08 11:10:27 -0300

    Projeto criado

---
+++ src/main/java/com/devalex/spring_ionic/ProjSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.devalex.spring_ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSpringIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devalex/spring_ionic/ProjSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalex.spring_ionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
