cf2dac2ddbe06d50aa5f722b320a4b9bb50aeee3
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-11 11:57:10 -0300

    Ajustes no endpoint /categorias/{id} e criação de exceções personalizadas

--- src/main/java/com/devalex/spring_ionic/services/CategoriaService.java
+++ src/main/java/com/devalex/spring_ionic/services/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.devalex.spring_ionic.domain.Categoria;
 import com.devalex.spring_ionic.repositories.CategoriaRepository;
+import com.devalex.spring_ionic.services.exceptions.ObjectNotFoundException;
 
 @Service 
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscarPorId(Integer id){
-		Optional<Categoria> obj =  repo.findById(id);
-		return obj.orElse(null);
-	}
+		
+	public Categoria buscarPorId(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 	
 	
 
