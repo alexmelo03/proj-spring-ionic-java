3586183304c6eab3d0ef21980d33a5f8c66c73a5
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-19 16:49:01 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/devalex/spring_ionic/resources/CategoriaResource.java
+++ src/main/java/com/devalex/spring_ionic/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.devalex.spring_ionic.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devalex.spring_ionic.domain.Categoria;
+import com.devalex.spring_ionic.dto.CategoriaDTO;
 import com.devalex.spring_ionic.services.CategoriaService;
 
 @RestController
@@ -48,5 +51,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		servico.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> buscarPorTodos() {
+		List<Categoria> list = servico.buscarPorTodos();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
 	}
 }
