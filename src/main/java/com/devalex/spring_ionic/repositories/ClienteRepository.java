f5c4b50a607881f15e2fddee5fda361a94825c30
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-25 17:28:32 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/devalex/spring_ionic/repositories/ClienteRepository.java
+++ src/main/java/com/devalex/spring_ionic/repositories/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.devalex.spring_ionic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devalex.spring_ionic.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
