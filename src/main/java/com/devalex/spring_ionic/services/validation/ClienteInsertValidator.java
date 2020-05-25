4a087e70dd9bae0e7a9720e4bdd43ecb5708b8d4
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-25 17:12:58 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

---
+++ src/main/java/com/devalex/spring_ionic/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,45 @@
+package com.devalex.spring_ionic.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.devalex.spring_ionic.domain.enums.TipoCliente;
+import com.devalex.spring_ionic.dto.ClienteNewDTO;
+import com.devalex.spring_ionic.resources.exceptions.FieldMessage;
+import com.devalex.spring_ionic.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+// inclua os testes aqui, inserindo erros na lista
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
