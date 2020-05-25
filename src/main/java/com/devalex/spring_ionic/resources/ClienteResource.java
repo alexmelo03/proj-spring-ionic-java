0509036f5b899e860392393b089b95c43b93df17
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-25 12:49:54 -0300

    PUT, DELETE e GET para Cliente:

--- src/main/java/com/devalex/spring_ionic/resources/ClienteResource.java
+++ src/main/java/com/devalex/spring_ionic/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.devalex.spring_ionic.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devalex.spring_ionic.domain.Cliente;
+import com.devalex.spring_ionic.dto.ClienteDTO;
 import com.devalex.spring_ionic.services.ClienteService;
 
 @RestController
@@ -24,4 +33,36 @@ public class ClienteResource {
     	return ResponseEntity.ok().body(obj);
 
 	}
+    
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> atualizar(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = servico.apartirDTO(objDto);
+		obj.setId(id);
+		obj = servico.atualizar(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		servico.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> buscarPorTodos() {
+		List<Cliente> list = servico.buscarPorTodos();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> buscaPorPagina(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = servico.buscaPorPagina(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
+	}	
 }
--- src/main/java/com/devalex/spring_ionic/services/ClienteService.java
+++ src/main/java/com/devalex/spring_ionic/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.devalex.spring_ionic.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.devalex.spring_ionic.domain.Cliente;
+import com.devalex.spring_ionic.dto.ClienteDTO;
 import com.devalex.spring_ionic.repositories.ClienteRepository;
+import com.devalex.spring_ionic.services.exceptions.DataIntegrityException;
 import com.devalex.spring_ionic.services.exceptions.ObjectNotFoundException;
 
 @Service 
@@ -21,6 +28,39 @@ public class ClienteService {
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
 	
+	public Cliente atualizar(Cliente obj) {
+		Cliente newObj = buscarPorId(obj.getId());
+		atualizarDados(newObj, obj);
+		return repo.save(newObj);
+	}
 	
-
+	public void delete(Integer id) {
+		buscarPorId(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> buscarPorTodos() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> buscaPorPagina(Integer pagina, Integer linhasPorPagina, String direcao, String ordemPor) {
+		PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordemPor);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente apartirDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void atualizarDados(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	
 }
