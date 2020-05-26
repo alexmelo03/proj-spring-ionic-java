9779278d503609bd3007f3487bf3077e533b2324
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-26 11:29:47 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/devalex/spring_ionic/services/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.devalex.spring_ionic.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.devalex.spring_ionic.domain.Categoria;
+import com.devalex.spring_ionic.domain.Produto;
+import com.devalex.spring_ionic.repositories.CategoriaRepository;
+import com.devalex.spring_ionic.repositories.ProdutoRepository;
+import com.devalex.spring_ionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	
+
+	public Produto buscarPorId(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> buscar(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,	String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+}
