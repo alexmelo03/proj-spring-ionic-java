b10fb33ffa7ab6d6f87d6f807e2eab13de5ab886
alexmelo03 <alexmello79@yahoo.com.br>
2020-06-01 11:35:54 -0300

    Email HTML

--- src/main/java/com/devalex/spring_ionic/services/EmailService.java
+++ src/main/java/com/devalex/spring_ionic/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.devalex.spring_ionic.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.devalex.spring_ionic.domain.Pedido;
@@ -9,5 +11,11 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+	
+	
 
 }
