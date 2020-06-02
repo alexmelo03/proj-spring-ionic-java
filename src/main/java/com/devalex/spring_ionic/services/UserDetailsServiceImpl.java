483c33cd4041af313cb0c2f8122b8221ac88b12a
alexmelo03 <alexmello79@yahoo.com.br>
2020-06-02 18:31:30 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/devalex/spring_ionic/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.devalex.spring_ionic.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devalex.spring_ionic.domain.Cliente;
+import com.devalex.spring_ionic.repositories.ClienteRepository;
+import com.devalex.spring_ionic.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
