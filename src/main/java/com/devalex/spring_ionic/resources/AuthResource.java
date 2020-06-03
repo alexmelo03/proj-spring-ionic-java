56c0f8c754deed5175c35284ae7676e22686db91
alexmelo03 <alexmello79@yahoo.com.br>
2020-06-03 11:04:06 -0300

    Esqueci a senha

--- src/main/java/com/devalex/spring_ionic/resources/AuthResource.java
+++ src/main/java/com/devalex/spring_ionic/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.devalex.spring_ionic.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devalex.spring_ionic.dto.EmailDTO;
 import com.devalex.spring_ionic.security.JWTUtil;
 import com.devalex.spring_ionic.security.UserSS;
+import com.devalex.spring_ionic.services.AuthService;
 import com.devalex.spring_ionic.services.UserService;
 
 @RestController
@@ -17,7 +21,10 @@ import com.devalex.spring_ionic.services.UserService;
 public class AuthResource {
 	
 	@Autowired
-	private JWTUtil jwtUtil; 
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 	
 	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -25,6 +32,12 @@ public class AuthResource {
 	String token = jwtUtil.generateToken(user.getUsername());
 	response.addHeader("Authorization", "Bearer " + token);
 	return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
 	}
 
 }
