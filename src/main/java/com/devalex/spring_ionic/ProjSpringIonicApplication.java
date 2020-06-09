194cdf65a112f11a69297199332cceeaef3299df
alexmelo03 <alexmello79@yahoo.com.br>
2020-06-09 19:15:52 -0300

    Enviando imagem via endpoint

--- src/main/java/com/devalex/spring_ionic/ProjSpringIonicApplication.java
+++ src/main/java/com/devalex/spring_ionic/ProjSpringIonicApplication.java
@@ -1,18 +1,12 @@
 package com.devalex.spring_ionic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devalex.spring_ionic.services.S3Service;
-
 @SpringBootApplication
 public class ProjSpringIonicApplication implements CommandLineRunner {
 	
-	@Autowired
- 	private S3Service s3Service;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjSpringIonicApplication.class, args);
 	}
@@ -20,7 +14,7 @@ public class ProjSpringIonicApplication implements CommandLineRunner {
 		
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\temp\\fotos\\alex.jpg");
+		
 	
 	}
 
--- src/main/java/com/devalex/spring_ionic/services/ClienteService.java
+++ src/main/java/com/devalex/spring_ionic/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.devalex.spring_ionic.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.devalex.spring_ionic.domain.Cidade;
 import com.devalex.spring_ionic.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 		
 	public Cliente buscarPorId(Integer id) {
 		
@@ -104,6 +109,10 @@ public class ClienteService {
 	private void atualizarDados(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 	
